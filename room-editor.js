8944ea54c066a67955a515352854d6efe25e821a
David Morgan <david.r.morgan.92@gmail.com>
2026-03-07 17:07:55 +0000

    hover sight blockers

--- room-editor.js
+++ room-editor.js
@@ -4,6 +4,10 @@ class RoomEditor {
     this.editSightBlockers = false
 
     this.canvasManager.addRenderOperation(this.renderSightBlockers.bind(this))
+
+    this.canvasManager.canvas.addEventListener('mousemove', event => this.handleMouseMove(event))
+
+    this.hoveredSightBlocker = null
   }
 
   toggleEditSightBlockers(show) {
@@ -30,6 +34,42 @@ class RoomEditor {
         blocker.height * scaleFactor)
     }
 
+    if (this.hoveredSightBlocker) {
+      this.canvasManager.ctx.strokeStyle = 'red'
+      this.canvasManager.ctx.lineWidth = 2
+      this.canvasManager.ctx.strokeRect(
+        this.hoveredSightBlocker.x * scaleFactor + offset.x,
+        this.hoveredSightBlocker.y * scaleFactor + offset.y,
+        this.hoveredSightBlocker.width * scaleFactor,
+        this.hoveredSightBlocker.height * scaleFactor)
+    }
+
     this.canvasManager.ctx.restore()
+  }
+
+  handleMouseMove(event) {
+    if (!this.editSightBlockers)
+      return
+
+    const scaleFactor = this.canvasManager.getScaleFactor()
+    const offset = this.canvasManager.getOffset()
+
+    const prevHovered = this.hoveredSightBlocker
+    this.hoveredSightBlocker = null
+
+    for (const blocker of this.canvasManager.compositor.sightBlockers) {
+      if (event.offsetX >= blocker.x * scaleFactor + offset.x &&
+          event.offsetX <= (blocker.x + blocker.width) * scaleFactor + offset.x &&
+          event.offsetY >= blocker.y * scaleFactor + offset.y &&
+          event.offsetY <= (blocker.y + blocker.height) * scaleFactor + offset.y) {
+        this.hoveredSightBlocker = blocker
+        break
+      }
+    }
+
+    if (prevHovered !== this.hoveredSightBlocker)
+      this.canvasManager.scheduleRender()
+
+    this.canvasManager.canvas.style.cursor = this.hoveredSightBlocker ? 'pointer' : 'grab'
   }
 }
