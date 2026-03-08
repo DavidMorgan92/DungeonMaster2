2d0938e2ddbd7a23d0a3afba65cbbdae93b451fb
David Morgan <david.r.morgan.92@gmail.com>
2026-03-08 21:59:29 +0000

    hover heroes

--- scripts/hero-manager.js
+++ scripts/hero-manager.js
@@ -6,6 +6,11 @@ class HeroManager {
     this.canvasManager.addRenderOperation(this.render.bind(this))
 
     this.imageLoader = new ImageLoader()
+
+    this.canvasManager.canvas.addEventListener('mousemove', event => this.handleMouseMove(event))
+
+    this.hoveredHero = null
+    this.dragging = false
   }
 
   render() {
@@ -26,9 +31,54 @@ class HeroManager {
     }
 
     this.canvasManager.ctx.restore()
+
+    if (this.hoveredHero)
+      this.renderHoveredHero(scaleFactor, offset)
+  }
+
+  renderHoveredHero(scaleFactor, offset) {
+    const radius = (Math.max(this.hoveredHero.icon.width, this.hoveredHero.icon.height) + 15) * scaleFactor / 2
+    const screenPoint = CoordinateUtils.worldToScreen(this.hoveredHero, scaleFactor, offset)
+
+    this.canvasManager.ctx.save()
+    this.canvasManager.ctx.strokeStyle = 'white'
+    this.canvasManager.ctx.lineWidth = 3
+    this.canvasManager.ctx.beginPath()
+    this.canvasManager.ctx.arc(screenPoint.x, screenPoint.y, radius, 0, Math.PI * 2)
+    this.canvasManager.ctx.stroke()
+    this.canvasManager.ctx.restore()
   }
 
   async changeHeroIcon(index) {
     await this.imageLoader.load(this.heroes[index].icon)
+  }
+
+  handleMouseMove(event) {
+    if (!this.dragging) {
+      const prevHovered = this.hoveredHero
+      this.hoveredHero = null
+
+      const scaleFactor = this.canvasManager.getScaleFactor()
+      const offset = this.canvasManager.getOffset()
+
+      for (const hero of this.heroes) {
+        const heroRect = {
+          x: hero.x - hero.icon.width / 2,
+          y: hero.y - hero.icon.height / 2,
+          width: hero.icon.width,
+          height: hero.icon.height,
+        }
+
+        const screenRect = CoordinateUtils.worldRectToScreen(heroRect, scaleFactor, offset)
+
+        if (CoordinateUtils.pointInRect({ x: event.offsetX, y: event.offsetY }, screenRect)) {
+          this.hoveredHero = hero
+          break
+        }
+      }
+
+      if (prevHovered !== this.hoveredHero)
+        this.canvasManager.scheduleRender()
+    }
   }
 }
