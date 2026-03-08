573fa47edb7413066b05a2942e2b27b9f6d1e0e7
David Morgan <david.r.morgan.92@gmail.com>
2026-03-08 12:07:21 +0000

    change background image

---
+++ background-image-loader.js
@@ -0,0 +1,29 @@
+class BackgroundImageLoader {
+  constructor (backgroundElement) {
+    this.backgroundElement = backgroundElement
+  }
+
+  async load() {
+    const [fileHandle] = await window.showOpenFilePicker({
+      multiple: false,
+      types: [{
+        description: 'Image files',
+        accept: { 'image/*': ['.png', '.jpg', '.jpeg'] },
+      }],
+    })
+
+    const file = await fileHandle.getFile()
+    const content = await this.fileToBase64(file)
+
+    this.backgroundElement.src = content
+  }
+
+  fileToBase64(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      reader.onload = () => resolve(reader.result)
+      reader.onerror = reject
+      reader.readAsDataURL(file)
+    })
+  }
+}
